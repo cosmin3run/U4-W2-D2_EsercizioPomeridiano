1303a8acf964cc19a76dc4971606c4c7351ca460
Cosmin <kosmyn1994@gmail.com>
2024-01-16 15:35:53 +0100

    es1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        /*System.out.println("Esercizio1_U4-W2-D2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Numero di parole da inserire");
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> parole = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Alla posizione " + (i + 1) + "che parola vuoi inserire?");
+            String str = scanner.nextLine();
+            parole.add(str);
+        }
+
+        System.out.println("Le parole duplicate sono:");
+        Iterator<String> duplicate = parole.iterator();
+        while (duplicate.hasNext()) {
+            String duplicata = duplicate.next();
+            if (!parole.add(duplicata)) {
+                System.out.println(duplicata);
+            }
+        }
+        System.out.println();*/
+
+
+        System.out.println("Esercizio2");
+
+    }
+}
